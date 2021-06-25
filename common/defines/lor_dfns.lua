44dc5c714acddde9cabb1905c93a82c3c4523a1f
vsevoloden <80573827+vsevoloden@users.noreply.github.com>
2021-06-25 09:56:42 +0300

    Update lor_dfns.lua

--- common/defines/lor_dfns.lua
+++ common/defines/lor_dfns.lua
@@ -1,3 +1,3 @@
 NDefines_Graphics.NGraphics.POLITICAL_GRID_SMALL_BOX_LIMIT = 999
 NDefines_Graphics.NGraphicz.CAMERA_MIN_HEIGHT = 25.0
-NDefines_Graphics.NGraphicz.CAMERA_MAX_HEIGHT = 3050.0
+NDefines_Graphics.NGraphicz.CAMERA_MAX_HEIGHT = 3000.0
